5f4c4d597b93312962e5858444a6710bf8554048
Kelly Nelson <nelson20@gmail.com>
2016-11-28 12:52:43 -0800

    configured production mongodb url with creds

--- config/env/production.js
+++ config/env/production.js
@@ -8,10 +8,10 @@ module.exports = {
   },
   port: process.env.PORT || 8443,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'ec2-107-21-30-106.compute-1.amazonaws.com') + '/mean',
     options: {
-      user: '',
-      pass: ''
+      user: 'hymnsbgadmin',
+      pass: 'solo123'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false

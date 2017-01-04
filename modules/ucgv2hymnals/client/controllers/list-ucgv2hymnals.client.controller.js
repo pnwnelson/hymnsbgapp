e9872a632e03f3bf71971dd0a55a41b2c354ed22
Kelly Nelson <nelson20@gmail.com>
2017-01-04 12:34:55 -0800

    fix ucgv2 list view bug

--- modules/ucgv2hymnals/client/controllers/list-ucgv2hymnals.client.controller.js
+++ modules/ucgv2hymnals/client/controllers/list-ucgv2hymnals.client.controller.js
@@ -7,12 +7,16 @@
 
   Ucgv2hymnalsListController.$inject = ['Ucgv2hymnalsService','$state'];
 
-  var vm = this;
-
   function Ucgv2hymnalsListController(Ucgv2hymnalsService, $state) {
-    
+    var vm = this;
 
     vm.ucgv2hymnals = Ucgv2hymnalsService.query();
+
+    vm.search = function() {
+      $state.go('ucgv2hymnals.view', { ucgv2hymnalId: vm.searchTerm });
+    };
+  }
+}());
 
   // function search(Ucgv2hymnalsService, req, res, next) {
   //   var searchTerm = {};
@@ -23,9 +27,3 @@
   //     }
   //     req.jsonp(ucgv2hymnals);
   //   });
-
-    vm.search = function() {
-      $state.go('ucgv2hymnals.view', { ucgv2hymnalId: vm.searchTerm });
-    };
-  }
-}());
